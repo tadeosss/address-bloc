7f2bf39a1035ee4ad8be1db1fab79c83c882538e
Sandy Tadeo <stadeo17@gmail.com>
2016-02-22 18:23:35 +0000

    Assignment 20 Address Bloc: Menu completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -15,7 +15,8 @@ class MenuController
        puts "2 - Create an entry"
        puts "3 - Search for an entry"
        puts "4 - Import entries from a CVS"
-       puts "5 - Exit"
+       puts "5 - View Entry n"
+       puts "6 - Exit"
        print "Enter your selection: "
        
        selection = gets.to_i
@@ -39,6 +40,10 @@ class MenuController
        read_csv
        main_menu
      when 5
+       system "clear"
+       entry_n_submenu
+       main_menu
+     when 6
        puts "Good-bye!"
 
        exit(0)
@@ -112,5 +117,19 @@ class MenuController
      end
    end
    
+   def entry_n_submenu
+       print "Enter number to view: "
+       selection = gets.chomp.to_i
+       
+       if selection < @address_book.entries.count
+           puts @address_book.entries[selection]
+           puts "Press enter to return to the main menu"
+           gets.chomp
+           system "clear"
+       else
+           puts "#{selection} is not a valid input."
+           entry_n_submenu
+       end
+   end
    
 end
